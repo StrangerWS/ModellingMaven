8bc36f4d06554c122be8d5b2ab918c7cce898dca
StrangerWS <strangernf@gmail.com>
2018-02-02 13:42:23 +0400

    alpha

--- src/main/java/com/strangerws/modelling/Task.java
+++ src/main/java/com/strangerws/modelling/Task.java
@@ -7,32 +7,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Task {
-    public static final double N = 1000;
+    public static final double N = 200;
     public static final double t0 = 0;
     public static final double h = 0.01;
     public static final double k = 0.01;
     public static final double nFinal = N * 0.95;
+    public static final double tFinal = N * h - h + t0;
 
     public static double func(double x) {
         return k * x * (N - x);
     }
-    public static Pair<List<Double>, List<Double>> task1(){
+
+
+    public static Pair<List<Double>, List<Double>> task1() {
         System.out.print("Enter a: ");
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         List<Double> xArr = new ArrayList<Double>();
         List<Double> yArr = new ArrayList<Double>();
-        double tPrev = a;
-        double tFinal = 0;
-        for (int i = 0; i <= N; i++) {
-            xArr.add(tPrev);
-            yArr.add((double)i);
-            tPrev += h*func(tPrev);
-            if (i == nFinal) {tFinal = tPrev;}
+        double funcPrev = a;
+        double funcFinal = 0;
+        double tAnswer = 0;
+
+        for (double i = 0; i <= tFinal; i += h) {
+            xArr.add(i);
+            yArr.add(funcPrev);
+            if (funcPrev >= nFinal && funcFinal == 0) {
+                funcFinal = funcPrev;
+                tAnswer = i;
+            }
+            funcPrev += h * func(funcPrev);
         }
 
-        System.out.println("x: 0.95 * N; \nt = "+tFinal);
 
-        return new Pair<List<Double>, List<Double>>(xArr, yArr);
+        System.out.println("f(0.95 * N) = " + funcFinal + "; \nt = " + tAnswer);
+
+        return new Pair<List<Double>, List<Double>>(yArr, xArr);
     }
 }
